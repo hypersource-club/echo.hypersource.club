b6fd851113ae94b477bc28c44890beae64d84dc2
Joseph Werle <joseph.werle@gmail.com>
2019-04-27 16:31:46 -0400

    refactor(index.js): Make a module

--- index.js
+++ index.js
@@ -4,40 +4,37 @@ const debug = require('debug')('echo.hypersource.club')
 const pump = require('pump')
 const ram = require('random-access-memory')
 
-const { PORT = 3000 } = process.env
-const server = hypersource.createServer(onrequest)
+function createServer(opts) {
+  const server = hypersource.createServer(onrequest)
 
-server.on('error', debug)
-server.listen(parseInt(PORT), (err) => {
-  if (err) {
-    console.error('ERR', err)
-  } else {
-    console.log('Listening on', server.address())
-  }
-})
+  server.on('error', debug)
 
-function onrequest(req, res) {
-  console.log('%s %s', req.method, req.url)
-  const source = hypercore(ram, req.key, req)
-  const echo = hypercore(ram, res.key, res)
+  return server
 
-  source.on('error', debug)
-  echo.on('error', debug)
+  function onrequest(req, res) {
+    const source = hypercore(ram, req.key, req)
+    const echo = hypercore(ram, res.key, res)
 
-  req.on('error', debug)
-  res.on('error', debug)
+    source.on('error', debug)
+    echo.on('error', debug)
 
-  source.replicate(req)
-  source.update(() => {
-    pump(
-      source.createReadStream(),
-      echo.createWriteStream(),
-      () => echo.replicate(res).once('close', onclose).on('end', onclose)
-    )
-  })
+    req.on('error', debug)
+    res.on('error', debug)
 
-  function onclose() {
-    source.close()
-    echo.close()
+    source.replicate(req)
+    source.update(() => {
+      pump(
+        source.createReadStream(),
+        echo.createWriteStream(),
+        () => echo.replicate(res).once('close', onclose).on('end', onclose)
+      )
+    })
+
+    function onclose() {
+      source.close()
+      echo.close()
+    }
   }
 }
+
+module.exports = createServer
